645bc3703f94cfd8d1df40fd6a501263d14c0172
Girik <68961075+Gyryk@users.noreply.github.com>
2023-06-25 00:42:16 +0530

    Added project files
    Did all the work locally, single commit gang

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Weather
-A simple VueJS website that shows you the weather for any location you want
-It uses Node.JS for the backend, and cookies to store the last place you looked up so that when you open the website again it automatically shows you the weather there
+## A very basic weather app
+
+#### A simple VueJS/NodeJS website that shows you the weather for any location you want
+It uses Node for the backend, Vue for the frontend, and cookies to store the last place you looked up so that when you open the website again it automatically shows you the weather there. <br>
+The background changes based on temperature alone, I might add weather conditions as well. <br>
+This project has mainly been an adventure in responsive design for me
+> I know this is pretty much the most basic project in existence, but I just had to make it 😭 <br>
+> Don't worry, I won't be making the to-do list as well... No one actually uses them anyways
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,191 @@
+<template>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 24 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="City Name" v-model="query" @keypress.enter="fetchWeather()"/>
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location"> {{weather.name}}, {{weather.sys.country}} </div>
+          <div class="date"> {{getDate()}} <br> {{getDay()}} </div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temperature"> {{Math.round(weather.main.temp)}}°C </div>
+          <div class="type"> {{weather.weather[0].main}} </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      api_key: 'ae638e144bef6826617cdae9bf42b110',
+      baseURL: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  mounted() {
+    let decodedCookie = decodeURIComponent(document.cookie);
+    if (decodedCookie !== "") {
+      this.query = decodedCookie;
+      this.fetchWeather();
+    }
+    else
+    {
+      this.query = "London, GB"
+      this.fetchWeather();
+    }
+  },
+  methods: {
+    fetchWeather() {
+      fetch(`${this.baseURL}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(result => {
+          return result.json();
+        }).then(this.setResult);
+    },
+
+    setResult(result) {
+      this.weather = result;
+      document.cookie = `${this.weather.name}, ${this.weather.sys.country}`
+    },
+
+    getDate() {
+      let d = new Date();
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${month} ${date}, ${year}`
+    },
+    getDay() {
+      let d = new Date();
+      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      let day = days[d.getDay()];
+
+      return `${day}`
+    }
+  }
+}
+</script>
+
+<style>
+  *
+  {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body
+  {
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  #app
+  {
+    background-image: url("./assets/cold.jpg");
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.5s;
+  }
+  #app.warm
+  {
+    background-image: url("./assets/warm.jpg");
+  }
+
+  main
+  {
+    min-height: 100vh;
+    padding: 25px;
+
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.0625), rgba(0, 0, 0, 0.75));
+  }
+
+  .search-box
+  {
+    width: 100%;
+    margin-bottom: 32px;
+  }
+
+  .search-box .search-bar
+  {
+    width: 100%;
+    display: block;
+    padding: 16px;
+
+    color: #222222;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    background-color: rgba(255, 255, 255, 0.5);
+
+    border-radius: 16px 0px 16px 0px;
+    transition: 0.25s;
+  }
+  .search-box .search-bar:focus
+  {
+    border-radius: 0px 16px 0px 16px;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.75)
+  }
+
+  .location-box .location
+  {
+    color: #ffffff;
+    font-size: 32px;
+    font-weight: 600;
+    text-align: center;
+    text-shadow: 2px 3px rgba(0, 0, 0, 0.25);
+  }
+
+  .location-box .date
+  {
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: 200;
+    text-align: center;
+  }
+
+  .weather-box
+  {
+    text-align: center;
+  }
+
+  .weather-box .temperature
+  {
+    display: inline-block;
+    padding: 12px 24px;
+    color: #ffffff;
+
+    font-size: 96px;
+    font-weight: 800;
+    text-shadow: 4px 6px rgba(0, 0, 0, 0.25);
+    font-family: 'Roboto', sans-serif;
+
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 24px;
+    margin: 30px 0px;
+    box-shadow: 4px 8px rgba(0, 0, 0, 0.125);
+  }
+
+  .weather-box .type
+  {
+    color: #ffffff;
+
+    font-size: 48px;
+    font-weight: 600;
+    text-shadow: 3px 5px rgba(0, 0, 0, 0.25);
+  }
+</style>
